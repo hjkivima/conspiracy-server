d700f14afd3cf054cb2507db51c9d5b3814d9805
hjkivima <hjkivima227985@gmail.com>
2023-04-20 09:14:57 +0300

    init

---
+++ database.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+const connectToMongoDB = async function () {
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('DB connection successfull');
+};
+
+module.exports = connectToMongoDB;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socketio = require('socket.io');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = express();
+const cors = require('cors');
+const connectToDB = require('./database');
+app.use(cors());
+
+const port = process.env.PORT || 9000;
+const expressServer = app.listen(port);
+
+connectToDB();
+
+const io = socketio(expressServer, {
+  cors: {
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+io.on('connect', socket => {
+  console.log('Connected');
+
+  socket.on('connection_ready', () => {
+    socket.emit('hello', 'You are connected');
+  });
+
+  socket.on('dataFromClient', data => {
+    //
+    console.log(data);
+  });
+});
